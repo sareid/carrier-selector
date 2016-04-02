a21ebc645a9a81dc7d99608dca80d0bf7bdb896d
sareid <andrew.reid@flatironschool.com>
2016-04-02 09:18:15 -0400

    jquery fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery_ujs
 //= require jquery
 //= require bootstrap
 //= require jquery.fittext
