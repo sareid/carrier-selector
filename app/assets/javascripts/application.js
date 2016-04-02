fd0fa95806e8cad614280b327b100c6cfae6dc3a
sareid <andrew.reid@flatironschool.com>
2016-04-02 09:32:29 -0400

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
-//= require jquery_ujs
 //= require jquery
+//= require jquery_ujs
+//= require bootstrap
 //= require jquery.fittext
 //= require jquery.easing.min
 //= require Chart
