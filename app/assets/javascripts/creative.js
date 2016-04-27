8e79f67a96b0df2e7527b038100b5a9f83adf20b
sareid <andrew.reid@flatironschool.com>
2016-04-26 23:48:57 -0400

    Edit js

--- app/assets/javascripts/creative.js
+++ app/assets/javascripts/creative.js
@@ -28,13 +28,13 @@
         $('.navbar-toggle:visible').click();
     });
 
-    // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
+    // // Fit Text Plugin for Main Header
+    // $("h1").fitText(
+    //     1.2, {
+    //         minFontSize: '35px',
+    //         maxFontSize: '65px'
+    //     }
+    // );
 
     // Offset for Main Navigation
     $('#mainNav').affix({
