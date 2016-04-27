d68cd5b68897c6ce11f4091a995f229adc1ff6c0
sareid <andrew.reid@flatironschool.com>
2016-04-27 14:31:47 -0400

    Fixes comparison maps on home

--- app/assets/javascripts/compare_maps.js
+++ app/assets/javascripts/compare_maps.js
@@ -51,8 +51,6 @@ $('#comparison').ready(function() {
             return d.properties.name +": " + numberWithCommas(d.properties.population);
           })
 
-         debugger;
-
         svg.append("path")
             .attr("class", "states")
             .datum(topojson.feature(us, us.objects.states))
@@ -70,106 +68,5 @@ $('#comparison').ready(function() {
 
       }
     }
-
-
-  // $.getJSON('carriers/usData', function(data){
-  //   drawMap(data);
-  // })
-
-  // $('.carrier-map').click(function(event){
-  //   event.preventDefault();
-  //   $.ajax({
-  //     type: "GET",
-  //     contentType: "application/json; charset=utf-8",
-  //     url: 'carriers/data',
-  //     data: {'id': $(this).data('params-id')},
-  //     dataType: 'json',
-  //     success: function (carrierData) {
-  //       drawCities(carrierData);
-  //     },
-  //       error: function (result) {
-  //       error();
-  //     }
-  //   });
-  // })
-
-  // function numberWithCommas(x) {
-  //   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  // }
-
-  // function drawMap(data){
-
-  //   var width = 960;
-  //   var height = 510;
-
-  //   var radius = d3.scale.sqrt()
-  //       .domain([0, 1e6])
-  //       .range([0, 8]);
-
-  //   var path = d3.geo.path();
-
-  //   var svg = d3.select("#metro-compare").append("svg")
-  //       .attr("width", width)
-  //       .attr("height", height);
-
-  //   ready(data);
-
-  //   function numberWithCommas(x) {
-  //     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  //   }
-
-  //   function ready(data) {
-
-
-  //     svg.append("path")
-  //         .attr("class", "states")
-  //         .datum(topojson.feature(data, data.objects.states))
-  //         .attr("d", path)
-
-  //   }
-  // }
-
-  // function drawCities(cityData) {
-
-  //   debugger;
-
-  //   var width = 960;
-  //   var height = 510;
-
-  //   var radius = d3.scale.sqrt()
-  //       .domain([0, 1e6])
-  //       .range([0, 8]);
-
-  //   var path = d3.geo.path();
-
-  //   var svg = d3.select("#metro-compare").append("svg")
-  //       .attr("width", width)
-  //       .attr("height", height);
-
-  //   ready(cityData);
-
-
-  //   function ready(cityData) {
-
-
-  //     var tip = d3.tip()
-  //       .attr('class', 'd3-tip')
-  //       .offset([-10, 0])
-  //       .html(function(d) {
-  //         return d.properties.name +": " + numberWithCommas(d.properties.population);
-  //       })
-
-  //     svg.selectAll(".symbol")
-  //       .data(cityData.features.sort(function(a, b) { return b.properties.population - a.properties.population; }))
-  //       .enter().append("path")
-  //       .attr("class", "symbol")
-  //       .attr("d", path.pointRadius(function(d) { return radius(d.properties.population); }))
-  //       .on('mouseover', tip.show)
-  //       .on('mouseout', tip.hide);
-
-  //     svg.call(tip);
-  //   }
-
-  // }
 });
 
